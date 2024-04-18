0c97b82b6d21f0c44f351ee59988bb9d069bc35e
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-18 10:34:45 +0900

    Dynamic Change with AnyLayout structure

--- DemoProjectApp/DemoProjectApp/ContentView.swift
+++ DemoProjectApp/DemoProjectApp/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var myLayout: AnyLayout = AnyLayout(VStackLayout()) //AnyLayout프로토콜사용
     var body: some View {
         VStack{
-            
+            myLayout  {
+                Image(systemName: "goforward.10")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit) //시뮬레이터 크기에 맞춰줌
+                Image(systemName: "goforward.15")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit) //시뮬레이터 크기에 맞춰줌
+            }
+            HStack {
+                Button(action: {
+                    myLayout = AnyLayout(HStackLayout())
+                }) {
+                    Text("HStack")
+                }
+                Button(action: {
+                    myLayout = AnyLayout(VStackLayout())
+                }) {
+                    Text("VStack")
+                }
+                Button(action: {
+                    myLayout = AnyLayout(ZStackLayout())
+                }) {
+                    Text("ZStack")
+                }
+
+            }
         }
     }
 }
