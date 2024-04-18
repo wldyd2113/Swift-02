7c942ff3d12911280db765162da664a01e054e6e
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-18 10:27:44 +0900

    Clear

--- DemoProjectApp/DemoProjectApp/ContentView.swift
+++ DemoProjectApp/DemoProjectApp/ContentView.swift
@@ -9,25 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{ //Stack뷰는 10개 정도에 view를 보여줄때 적합
-            HStack {
-                Group {
-                    Image(systemName: "goforward.10")
-                    Image(systemName: "goforward.15")
-                    Image(systemName: "goforward.30")
-                    Image(systemName: "goforward.30")
-                    Image(systemName: "goforward.30")
-                    Image(systemName: "goforward.30")
-                }
-                Group {
-                    Image(systemName: "goforward.30")
-                    Image(systemName: "goforward.30")
-                    Image(systemName: "goforward.30")
-                    Image(systemName: "goforward.30")
-                    Image(systemName: "goforward.30")
-                    Image(systemName: "goforward.30")
-                }
-            }
+        VStack{
+            
         }
     }
 }
