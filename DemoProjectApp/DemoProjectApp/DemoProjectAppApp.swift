50673a9d25f6e813176e7fdb9e845420974b8666
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-18 09:29:34 +0900

    커밋 메시지를 입력하세요

---
+++ DemoProjectApp/DemoProjectApp/DemoProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectAppApp.swift
+//  DemoProjectApp
+//
+//  Created by 차지용 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
