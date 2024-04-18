a3e9f97ed8b1dc3f7c40a982d6960a6ecdda117f
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-18 11:32:13 +0900

    GeometryReader

--- DemoProjectApp/DemoProjectApp/ContentView.swift
+++ DemoProjectApp/DemoProjectApp/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myLayout: AnyLayout = AnyLayout(VStackLayout()) //AnyLayout프로토콜사용
     var body: some View {
-        HStack{
-            Text("Hello World, how are you?")
-                .font(.largeTitle)
-                .border(Color.black)
-//                .frame(minWidth: 100, maxWidth: 300, minHeight: 100, maxHeight: 100, alignment: .center)
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center) //infinity얼마든지 커져봐!
+        VStack (alignment: .center){
+            GeometryReader { geometry in //$0은 임시변수여서
+                //$0 여기서는 쓸수 있는데 다른데서 사용불가
+                Text("Hello World, how are you?")
+                    .font(.largeTitle)
+                    .border(Color.black)
+                //                .frame(minWidth: 100, maxWidth: 300, minHeight: 100, maxHeight: 100, alignment: .center)
+                    .frame(minWidth: 0, maxWidth: geometry.size.width / 2, minHeight: 0, maxHeight: .infinity, alignment: .center) //infinity얼마든지 커져봐!
+            }
         }
         .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
