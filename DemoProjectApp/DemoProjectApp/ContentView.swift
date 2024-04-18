5709bb07925bda06a58d26f8645fb14be894201a
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-18 11:32:47 +0900

    Clear

--- DemoProjectApp/DemoProjectApp/ContentView.swift
+++ DemoProjectApp/DemoProjectApp/ContentView.swift
@@ -9,17 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .center){
-            GeometryReader { geometry in //$0은 임시변수여서
-                //$0 여기서는 쓸수 있는데 다른데서 사용불가
-                Text("Hello World, how are you?")
-                    .font(.largeTitle)
-                    .border(Color.black)
-                //                .frame(minWidth: 100, maxWidth: 300, minHeight: 100, maxHeight: 100, alignment: .center)
-                    .frame(minWidth: 0, maxWidth: geometry.size.width / 2, minHeight: 0, maxHeight: .infinity, alignment: .center) //infinity얼마든지 커져봐!
-            }
+        VStack {
+
         }
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
