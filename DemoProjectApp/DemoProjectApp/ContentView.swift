5b9b56fc31b3998e26a7e1a720f584855c51b366
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-18 11:16:15 +0900

    myLayout

--- DemoProjectApp/DemoProjectApp/ContentView.swift
+++ DemoProjectApp/DemoProjectApp/ContentView.swift
@@ -10,8 +10,16 @@ import SwiftUI
 struct ContentView: View {
     @State var myLayout: AnyLayout = AnyLayout(VStackLayout()) //AnyLayout프로토콜사용
     var body: some View {
-        VStack{
-
+        HStack{
+            Image(systemName: "airplane")
+            Text("Flight times: ")
+            Text("London")
+        }
+        .font(.largeTitle)
+        .frame(width: 300)
+        .lineLimit(1...4) //라인 제한
+        .transaction { transaction in
+            transaction.disablesAnimations = true //클로저 문법에서 in을 사용하지 않으면 익명 파라미터로 $0을 사용 ex)(transaction,transaction2) 인경우 $0, $1사용가능
         }
     }
 }
