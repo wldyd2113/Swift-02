8299f149ecdf4d126f710665c03e850a79fb6a36
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-19 09:19:33 +0900

    Binding

--- DemoProjectApp/DemoProjectApp/ContentView.swift
+++ DemoProjectApp/DemoProjectApp/ContentView.swift
@@ -5,26 +5,33 @@
 //  Created by 차지용 on 4/18/24.
 //
 
+//바인딩하는 역활 연결만해주는게 아니라
 import SwiftUI
+
 
 struct ContentView: View {
     @State private var wifiEnabled = true
     @State private var userName = ""
     var body: some View {
         VStack {
-            Toggle(isOn: $wifiEnabled){
-                Text("Enable Wi-Fi")
-            }
             TextField("Enter user name", text: $userName)
             Text(userName)
             Image(systemName: wifiEnabled ? "wifi" : "wifi.slash")
                 .font(.largeTitle)
-                .frame(height: 100)
+
+            WifiImageView(wifiEnabled2: $wifiEnabled)
         }
     }
 }
 
-
+struct WifiImageView: View {
+    @Binding var wifiEnabled2: Bool
+    var body: some View {
+        Toggle(isOn: $wifiEnabled2) {
+            Text("Enable Wi-Fi")
+        }
+    }
+}
 #Preview {
     ContentView()
 }
