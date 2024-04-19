f5d7ce35605cf39499b5cc641d8cbb67c68bf0e3
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-19 15:08:48 +0900

    Task detached

--- DemoProjectApp/DemoProjectApp/ContentView.swift
+++ DemoProjectApp/DemoProjectApp/ContentView.swift
@@ -5,7 +5,6 @@
 //  Created by 차지용 on 4/18/24.
 //
 
-
 import SwiftUI
 
 struct User: Codable {
@@ -17,76 +16,91 @@ struct Repository: Codable, Identifiable {
     let id: Int
     let name: String
     let description: String?
-    let stargazersCount: Int
 }
 
 actor GithubService {
     func fetchUser(username: String) async throws -> User {
-        let url = URL(string:"https://api.github.com/users/\(username)")!
-        
-        let(data, _) = try await URLSession.shared.data(from: url)
-        
+        let url = URL(string: "https://api.github.com/users/\(username)")!
+        let (data, _) = try await URLSession.shared.data(from: url)
         return try JSONDecoder().decode(User.self, from: data)
     }
     
     func fetchRepositories(username: String) async throws -> [Repository] {
         let url = URL(string: "https://api.github.com/users/\(username)/repos")!
-        
-        let(data, _) = try await URLSession.shared.data(from: url)
-        
+        let (data, _) = try await URLSession.shared.data(from: url)
         return try JSONDecoder().decode([Repository].self, from: data)
     }
 }
 
 struct ContentView: View {
-    @State private var username = "wldyd2113"
+    @State private var username = "jmbae"
     @State private var user: User?
     @State private var repositories: [Repository] = []
     let githubService = GithubService()
     
     var body: some View {
         VStack {
-            TextField("Github user,name:", text: $username)
+            TextField("Github username:", text: $username)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             Button("Fetch Data") {
                 Task {
                     do {
-                        async let fetchRepositoris = githubService.fetchRepositories(username: username)
                         async let fetchUser = githubService.fetchUser(username: username)
+                        async let fetchRepositoris = githubService.fetchRepositories(username: username)
                         
-                        user =  try await fetchUser
+                        user = try await fetchUser
                         repositories = try await fetchRepositoris
-                    }catch {
+                    } catch {
                         print("Error: \(error)")
                     }
                 }
             }
+            
             if let user = user {
                 AsyncImage(url: user.avatar_url) { image in
                     image.resizable()
-                }placeholder: {
+                } placeholder: {
                     ProgressView()
                 }
-                .frame(width: 100, height: 100)
-                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .frame(width:100, height:100)
+                .clipShape(Circle())
                 
                 Text(user.login)
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .font(.title)
             }
+            
             List(repositories) { repo in
                 VStack(alignment: .leading) {
                     Text(repo.name)
                         .font(.headline)
-                    Text(repo.name)
+                    Text(repo.description ?? "No description")
                         .font(.subheadline)
-                    Text("Stars: \(repo.stargazersCount)")
                 }
-                
+            }
+            
+            Button("Fetch Data in Background") {
+                Task.detached {
+                    do {
+                        let service = GithubService()
+                        try await withThrowingTaskGroup(of: Void.self) { group in
+                            group.addTask {
+                                repositories = try await service.fetchRepositories(username: username)
+                            }
+                            group.addTask {
+                                user = try await service.fetchUser(username: username)
+                            }
+                            try await group.waitForAll()
+                        }
+                    } catch {
+                        DispatchQueue.main.async {
+                            print("\(error)")
+                        }
+                    }
+                }
             }
         }
     }
-
 }
 
 
