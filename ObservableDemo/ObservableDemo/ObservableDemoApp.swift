96a7515a411b7bf574ca9c1430a0af1c8e38f3ef
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-19 17:12:06 +0900

    Intialize Project ObservableDemo

---
+++ ObservableDemo/ObservableDemo/ObservableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableDemoApp.swift
+//  ObservableDemo
+//
+//  Created by 차지용 on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObservableDemo/ObservableDemo/TimerData.swift
@@ -0,0 +1,8 @@
+//
+//  TimerData.swift
+//  ObservableDemo
+//
+//  Created by 차지용 on 4/19/24.
+//
+
+import Foundation
