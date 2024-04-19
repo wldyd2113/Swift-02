877f7c460603da9dc8c84330c03209bba225528f
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-19 09:49:37 +0900

    increment counter example for combine framework

--- DemoProjectApp/DemoProjectApp/ContentView.swift
+++ DemoProjectApp/DemoProjectApp/ContentView.swift
@@ -8,15 +8,20 @@
 //바인딩하는 역활 연결만해주는게 아니라
 import SwiftUI
 
+//ObservableObject프로토콜은 해당 클래스의 인스턴스를 감시하고 있다가 값이 변경되면 업데이트 해줌
+//@Published :클래스 해당인스턴스를 감시하고 있다
 class DemoData: ObservableObject {
     @Published var userCount = 0
-    @Published var cuurentUser = ""
+//    @Published var cuurentUser = ""
     
-    init() {
-        updateData()
-    }
-    func updateData() {
-        
+//    init() {
+//        updateData()
+//    }
+//    func updateData() {
+//        
+//    }
+    func increment() {
+        userCount += 1
     }
 }
 
@@ -24,7 +29,13 @@ struct ContentView: View {
     @StateObject var demoData: DemoData = DemoData()
     var body: some View {
         VStack {
-            Text("\(demoData.cuurentUser), you are user number \(demoData.userCount)")
+            Text(" you are user number \(demoData.userCount)")
+            Button(action: {
+                demoData.increment()
+            }) {
+                Text("Incremnet")
+                    .font(.largeTitle)
+            }
         }
     }
 }
