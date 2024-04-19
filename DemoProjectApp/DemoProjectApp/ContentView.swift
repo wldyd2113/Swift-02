1f5db2f2713c160afe6ba1e0adf062988b427402
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-19 09:37:36 +0900

    ObservableObject

--- DemoProjectApp/DemoProjectApp/ContentView.swift
+++ DemoProjectApp/DemoProjectApp/ContentView.swift
@@ -8,13 +8,23 @@
 //바인딩하는 역활 연결만해주는게 아니라
 import SwiftUI
 
+class DemoData: ObservableObject {
+    @Published var userCount = 0
+    @Published var cuurentUser = ""
+    
+    init() {
+        updateData()
+    }
+    func updateData() {
+        
+    }
+}
 
 struct ContentView: View {
-    @State private var wifiEnabled = true
-    @State private var userName = ""
+    @StateObject var demoData: DemoData = DemoData()
     var body: some View {
         VStack {
-
+            Text("\(demoData.cuurentUser), you are user number \(demoData.userCount)")
         }
     }
 }
