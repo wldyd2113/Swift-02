a3d7c32b7b9c058280aad59a5cdad565dcc6daab
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-19 16:22:27 +0900

    주석 추가

--- LifecycleDemo/LifecycleDemo/FirstTabView.swift
+++ LifecycleDemo/LifecycleDemo/FirstTabView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct FirstTabView: View {
     var body: some View {
         Text("View One")
-            .onAppear(perform: {
+            .onAppear(perform: { //이화면에 왔을때 반응
                 print("onAppear triggred")
             })
-            .onDisappear(perform: {
+            .onDisappear(perform: { //다른화면이로 이동했을때 반응
                 print("onDisappear triggred")
             })
     }
