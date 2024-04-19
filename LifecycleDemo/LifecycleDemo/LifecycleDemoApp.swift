ecc0911afefec46e225fd0d027495274dd127039
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-19 16:46:42 +0900

    phase onChange

--- LifecycleDemo/LifecycleDemo/LifecycleDemoApp.swift
+++ LifecycleDemo/LifecycleDemo/LifecycleDemoApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct LifecycleDemoApp: App {
+    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        //window에서 상태변화를 보여줌
+        .onChange(of: scenePhase) { (oldScenePharse, newScenePharse) in
+            switch newScenePharse {
+            case .active:
+                print("Active \(oldScenePharse)")
+            case .inactive:
+                print("Inactive \(oldScenePharse)")
+            case .background:
+                print("Background")
+            default:
+                print("Unknown scenephase")
+            }
         }
     }
 }
