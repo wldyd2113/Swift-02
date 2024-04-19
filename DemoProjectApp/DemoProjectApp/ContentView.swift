980048f6864faef2d7600240558cf376d143b33f
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-19 13:27:50 +0900

    Clear

--- DemoProjectApp/DemoProjectApp/ContentView.swift
+++ DemoProjectApp/DemoProjectApp/ContentView.swift
@@ -11,37 +11,8 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
-    func doSomething (){
-        print("Start \(Date())")
-        let detachedTask = Task.detached(priority: .background) {
-            //            let result = await takesTooLong()
-            //            print("Date result: \(result)")
-            
-            let result = await takesTooLong()
-            print("Date result: \(result)")
-        }
-        if(!detachedTask.isCancelled){
-            detachedTask.cancel()
-        }
-        print("End \(Date())")
-    }
-    
-    func takesTooLong() async -> Date {
-        print("tasksTooLong starts")
-        sleep(5)
-        print("tasksTooLong")
-        return Date()
-    }
-    
-    
     var body: some View {
         VStack {
-            Button(action: {
-                doSomething()
-            }) {
-                Text("Do something")
-            }
         }
     }
 }
