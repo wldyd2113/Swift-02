4ad9fbb7d1e74399ecad2e51baa12186cfcc699e
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-22 09:12:11 +0900

    Project Create

---
+++ StorageDemo/StorageDemo/StorageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageDemoApp.swift
+//  StorageDemo
+//
+//  Created by 차지용 on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct StorageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
