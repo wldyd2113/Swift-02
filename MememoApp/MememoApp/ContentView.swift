ba3aad06a7b90b5a00a21582eb3cfb0a6330f4f3
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-22 15:27:49 +0900

    ContentView 구현

--- MememoApp/MememoApp/ContentView.swift
+++ MememoApp/MememoApp/ContentView.swift
@@ -31,10 +31,12 @@ struct Memo {
 class MemoStroe: ObservableObject {
     @Published var memos: [Memo] = []
     
+    //추가
     func addMemo(_ text: String, color: Color) {
         let memo: Memo = Memo(text: text, color: color, created: Date())
     }
     
+    //삭제
     func removeMemo(_ targetMemo: Memo) {
         if let index = memos.firstIndex(where: {$0.id == targetMemo.id}) {
             memos.remove(at: index)
@@ -45,9 +47,51 @@ class MemoStroe: ObservableObject {
 
 
 struct ContentView: View {
+    @ObservedObject var memoStore: MemoStroe = MemoStroe()
+    
+    @State var isSheetShowing: Bool = false
+    @State var memoText: String = ""
+    @State var memoColor: Color = .blue
+    let colors:[Color] = [.blue, .cyan, .purple, .yellow, .indigo]
+    
     var body: some View {
-        VStack {
-
+        NavigationStack {
+            List(memoStore.memos) { memo in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(memo.text)").font(.title)
+                        Text("\(memo.createdString)").font(.body).padding(.top)
+                    }
+                    Spacer()
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(memo.color)
+                .shadow(radius: 3)
+                .padding()
+                .contextMenu {
+                    ShareLink(item: memo.text)
+                    Button {
+                        memoStore.removeMemo(memo)
+                    } label: {
+                        Image(systemName: "trash.slash")
+                        Text("삭제")
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("mememo")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("추가") {
+                        memoText = ""
+                        isSheetShowing =  true
+                    }
+                }
+            }
+            .sheet(isPresented: $isSheetShowing) {
+                MemoAddView()
+            }
         }
     }
 }
