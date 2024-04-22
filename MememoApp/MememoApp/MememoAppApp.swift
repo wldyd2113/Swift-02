dd5cc98d619a63f7a35b40bb4c3c3efdeaceda4b
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-22 14:45:14 +0900

    Project Create

---
+++ MememoApp/MememoApp/MememoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MememoAppApp.swift
+//  MememoApp
+//
+//  Created by 차지용 on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MememoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MememoApp/MememoApp/MemoAddView.swift
@@ -0,0 +1,18 @@
+//
+//  MemoAddView.swift
+//  MememoApp
+//
+//  Created by 차지용 on 4/22/24.
+//
+
+import SwiftUI
+
+struct MemoAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoAddView()
+}
