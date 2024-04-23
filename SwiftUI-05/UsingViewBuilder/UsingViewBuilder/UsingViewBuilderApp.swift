6d2b60bf0360a2505cb36cb1739d14b39e7f6fa7
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 16:22:02 +0900

    New Project

---
+++ SwiftUI-05/UsingViewBuilder/UsingViewBuilder/UsingViewBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingViewBuilderApp.swift
+//  UsingViewBuilder
+//
+//  Created by 차지용 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingViewBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
