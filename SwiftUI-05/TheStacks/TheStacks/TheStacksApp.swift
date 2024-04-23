f10ce327cae945d23ddd2edd6b54c46e0ac9e935
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 13:13:48 +0900

    Project Create

---
+++ SwiftUI-05/TheStacks/TheStacks/TheStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheStacksApp.swift
+//  TheStacks
+//
+//  Created by 차지용 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TheStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
