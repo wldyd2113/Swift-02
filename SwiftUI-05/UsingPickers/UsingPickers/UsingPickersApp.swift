7d6d9ab98eba391f4b29f007bd3978d0ab49a009
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 15:38:29 +0900

    New Project

---
+++ SwiftUI-05/UsingPickers/UsingPickers/UsingPickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingPickersApp.swift
+//  UsingPickers
+//
+//  Created by 차지용 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingPickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
