9012504848dabec5a19f11c1c45fb66851513061
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 13:34:11 +0900

    Project Create

---
+++ SwiftUI-05/FormattedText/FormattedText/FormattedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormattedTextApp.swift
+//  FormattedText
+//
+//  Created by 차지용 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FormattedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
