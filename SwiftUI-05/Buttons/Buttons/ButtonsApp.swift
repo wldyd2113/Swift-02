474d24f9c7ee4068f2b4d908f8a2875a967af63c
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 14:17:13 +0900

    New Project

---
+++ SwiftUI-05/Buttons/Buttons/ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsApp.swift
+//  Buttons
+//
+//  Created by 차지용 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
