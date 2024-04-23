f86eaeb3fc5a2af01655614fb648a91a093df730
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 16:34:43 +0900

    주석 추가

--- SwiftUI-05/UsingViewBuilder/UsingViewBuilder/BlueCircle.swift
+++ SwiftUI-05/UsingViewBuilder/UsingViewBuilder/BlueCircle.swift
@@ -6,6 +6,11 @@
 //
 
 import SwiftUI
+/*
+ @ViewBuilder 특성을 사용하여 여러 개의 View를 반환할 수 있는 클로저를 받습니다.
+ 이 클로저에서 반환된 View는 self.content 속성에 할당되며,
+ 이를 통해 BlueCircle 구조체의 body 속성에서 사용할 수 있습니다.
+ */
 
 struct BlueCircle<Content:View>: View {
     let content: Content
