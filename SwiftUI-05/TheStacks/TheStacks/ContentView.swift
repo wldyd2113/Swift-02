85040553d75d464b6d59fe72d8d10a6eab1f605e
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 13:20:11 +0900

    The Stacks

--- SwiftUI-05/TheStacks/TheStacks/ContentView.swift
+++ SwiftUI-05/TheStacks/TheStacks/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack {//세로
+            Text("VStack Item 1!")
+            Text("VStack Item 2!")
+            Spacer()
+            Divider()
+                .background(.black)
+            Text("VStack Item 3!")
+            HStack { //가로
+                Text("HStack Item 1")
+                Divider()
+                    .background(.black)
+                Spacer()
+                Text("HStack Item 2")
+                Divider()
+                    .background(.black)
+                Spacer()
+                Text("HStack Item 3")
+            }
+            .background(.red)
+            ZStack { //겹치기
+                Text("ZStack Item 1")
+                    .padding()
+                    .background(.green)
+                    .opacity(0.8)
+                Text("ZStack Item 2")
+                    .padding()
+                    .background(.green)
+                    .offset(x: 80, y: -400) //상대 좌표 축만큼 이동
+            }
         }
-        .padding()
+        .background(.blue)
     }
 }
 
