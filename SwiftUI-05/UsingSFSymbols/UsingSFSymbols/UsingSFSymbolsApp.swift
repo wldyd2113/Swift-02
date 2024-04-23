372a63077b14ae12065df537bb27b90e33213cf0
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 16:42:04 +0900

    NEW Project

---
+++ SwiftUI-05/UsingSFSymbols/UsingSFSymbols/UsingSFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingSFSymbolsApp.swift
+//  UsingSFSymbols
+//
+//  Created by 차지용 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingSFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
