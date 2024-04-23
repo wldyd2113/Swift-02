d2faaf3c672ad6a6350b77853859902f0fbe37f2
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 13:48:31 +0900

    Formatted Text

--- SwiftUI-05/FormattedText/FormattedText/ContentView.swift
+++ SwiftUI-05/FormattedText/FormattedText/ContentView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var password = "1234"
+    @State private var someText = "initial text"
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+                .fontWeight(.medium)  //폰트 두께
+            
+        SecureField("Enter a password", text: $password)
+        Text("password enter: \(password)")
+            .italic()
+            
+        
+        TextField("Enter some text", text: $someText)
+            .padding()
+        Text(someText)
+            .font(.largeTitle)
+            .underline()
+            
+        Text("Changing text color and make it bold")
+            .foregroundStyle(.blue)
+        Text("Use kerning to change space between characters in the text")
+            .kerning(7)
+            Text("Changing baseline offset")
+                .baselineOffset(100)
+        Text("Strikethrough")
+            .strikethrough()
+            Text("This is a multiline text implemented in SwiftUI. The trailing modifier was added to the text. This text also implements multiple modifiers")
+                .background(.yellow)
+                .multilineTextAlignment(.trailing)
+                .lineSpacing(10)
         }
         .padding()
     }
