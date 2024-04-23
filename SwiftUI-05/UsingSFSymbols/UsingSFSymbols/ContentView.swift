7f11aa48064a42964071d6c969caf6f1394e5e18
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 17:19:10 +0900

    SF Symbols

--- SwiftUI-05/UsingSFSymbols/UsingSFSymbols/ContentView.swift
+++ SwiftUI-05/UsingSFSymbols/UsingSFSymbols/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var wifiSelecton = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Image(systemName: "c")
+                Image(systemName: "o")
+                Image(systemName: "o")
+                Image(systemName: "k")
+            }
+            .symbolVariant(.fill.circle)
+            .foregroundStyle(.yellow, /*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            .font(.title)
+            
+            HStack {
+                Image(systemName: "b.circle.fill")
+                Image(systemName: "o.circle.fill")
+                    .foregroundStyle(.red)
+                Image(systemName: "o.circle.fill")
+                    .imageScale(.large)
+                Image(systemName: "k.circle.fill")
+                    .accessibilityIdentifier("Letter K")
+
+            }
+            .foregroundStyle(.blue)
+            .font(.title)
+            .padding()
+            
+            HStack {
+                Image(systemName: "allergens")
+                Image(systemName: "ladybug")
+            }
+            .symbolVariant(.fill)
+            .symbolRenderingMode(.multicolor)
+            .font(.largeTitle)
+            
+            HStack {
+                Picker("Pick One", selection: $wifiSelecton) {
+                    Text("No Wifi").tag(0)
+                    Text("Searching").tag(1)
+                    Text("Wifi On").tag(2)
+                }
+                .pickerStyle(.segmented)
+                .frame(width: 240)
+                .padding(.horizontal)
+                Group {
+                    switch wifiSelecton {
+                    case 0:
+                        Image(systemName: "wifi")
+                            .symbolVariant(.slash)
+                    case 1:
+                        Image(systemName: "wifi")
+                            .symbolEffect(.variableColor.iterative.reversing)
+                    default:
+                        Image(systemName: "wifi")
+                            .foregroundStyle(.blue)
+                    }
+                }
+                .foregroundStyle(.secondary)
+                .font(.title)
+            }
         }
-        .padding()
     }
 }
 
