ccbea094dd27033056d993eae4be0a2ee8fbb3c2
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 17:24:21 +0900

    New Project

---
+++ SwiftUI-05/UIKitToSwoftUI/UIKitToSwoftUI/UIKitToSwoftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitToSwoftUIApp.swift
+//  UIKitToSwoftUI
+//
+//  Created by 차지용 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UIKitToSwoftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
