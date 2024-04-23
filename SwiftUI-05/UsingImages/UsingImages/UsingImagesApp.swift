9e81e0aba004172fbf7c37cd3092523299290a45
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 14:03:45 +0900

    Project Create

---
+++ SwiftUI-05/UsingImages/UsingImages/UsingImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingImagesApp.swift
+//  UsingImages
+//
+//  Created by 차지용 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
