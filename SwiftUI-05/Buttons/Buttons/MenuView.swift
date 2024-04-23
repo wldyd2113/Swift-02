0130570af3e645ef4610ad67eb2ffd752c58a3c7
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-23 15:26:19 +0900

    MenuButton

--- SwiftUI-05/Buttons/Buttons/MenuView.swift
+++ SwiftUI-05/Buttons/Buttons/MenuView.swift
@@ -1,31 +1,31 @@
-////
-////  MenuView.swift
-////  Buttons
-////
-////  Created by 차지용 on 4/23/24.
-////
 //
-//import SwiftUI
+//  MenuView.swift
+//  Buttons
 //
-//struct MenuView: View {
-//    var body: some View {
-//        Menu("Choose a country") {
-//            Button("Canada") {
-//                print("Select Canada")
-//            }
-//            Button("Maxico") {
-//                print("Select Maxico")
-//            }
-//            Button("USA") {
-//                print("Select Canada")
-//            }
-//        }
-//        .navigationTitle("USA")
-//    }
-//}
+//  Created by 차지용 on 4/23/24.
 //
-//#Preview {
-//    NavigationStack{
-//        MenuView()
-//    }
-//}
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Menu("Choose a country") {
+            Button("Canada") {
+                print("Select Canada")
+            }
+            Button("Maxico") {
+                print("Select Maxico")
+            }
+            Button("USA") {
+                print("Select Canada")
+            }
+        }
+        .navigationTitle("USA")
+    }
+}
+
+#Preview {
+    NavigationStack{
+        MenuView()
+    }
+}
