a9573184c1b0e99cee0fa65c431033dae1478a65
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-24 11:49:22 +0900

    EditableList

--- SwiftUI-05/EditableList/EditableList/ContentView.swift
+++ SwiftUI-05/EditableList/EditableList/ContentView.swift
@@ -6,16 +6,23 @@
 //
 
 import SwiftUI
+struct TodoItme: Identifiable {
+    let id = UUID()
+    var title: String
+}
 
 struct ContentView: View {
+    @State private var todos = [
+        TodoItme(title: "Eat"),
+        TodoItme(title: "Sleep"),
+        TodoItme(title: "Code"),
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List($todos) { todo in
+            TextField("Todo item", text: todo.title)
+
         }
-        .padding()
+        
     }
 }
 
