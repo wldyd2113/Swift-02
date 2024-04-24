e4203993f393a4f975eacd8c9cde0693ef2ddbde
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-24 13:26:42 +0900

    New Project

---
+++ SwiftUI-05/SearchList/SearchList/SearchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListApp.swift
+//  SearchList
+//
+//  Created by 차지용 on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
