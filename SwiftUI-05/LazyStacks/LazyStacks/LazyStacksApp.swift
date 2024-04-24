fba72f427c0fc6331d9b7a97f63f5f9958b7adb7
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-24 14:35:49 +0900

    New Project

---
+++ SwiftUI-05/LazyStacks/LazyStacks/LazyStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyStacksApp.swift
+//  LazyStacks
+//
+//  Created by 차지용 on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LazyStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
