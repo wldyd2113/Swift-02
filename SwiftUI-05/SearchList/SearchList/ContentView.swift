33aac453d5970521454003a916dd73c2da39c28e
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-24 13:43:20 +0900

    SearchList

--- SwiftUI-05/SearchList/SearchList/ContentView.swift
+++ SwiftUI-05/SearchList/SearchList/ContentView.swift
@@ -7,15 +7,35 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State var searchText = ""
+    let food:[Food] = Food.sampleFood
+    
+    var serachResults: [Food] {
+        if searchText.isEmpty {
+            return food
         }
-        .padding()
+        else {
+            return food.filter{
+                ($0.name.lowercased().contains(searchText.lowercased()))
+            }//filter 고차원함수 true면 실행
+        }
+    }
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(serachResults, id: \.self) { food in
+                    LabeledContent(food.name) {
+                        Text("\(food.category.rawValue)")
+                    }
+                }
+            }
+            .searchable(text: $searchText, prompt: "Serach for a food")
+            .navigationTitle("Food")
+            
+        }
     }
 }
 
