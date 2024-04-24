61d110d08e0d976f691c1303a8c2e4060f1e9d96
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-24 10:03:51 +0900

    New Project

---
+++ SwiftUI-05/WeScroll/WeScroll/WeScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeScrollApp.swift
+//  WeScroll
+//
+//  Created by 차지용 on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WeScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
