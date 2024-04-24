b447b135f616f30fa2a5a1da60e38c1e9a849bd5
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-24 11:02:35 +0900

    New Project

---
+++ SwiftUI-05/ListActions/ListActions/ListActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListActionsApp.swift
+//  ListActions
+//
+//  Created by 차지용 on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ListActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
