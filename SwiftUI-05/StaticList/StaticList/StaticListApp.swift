fbfb5c126413797660fb10c5a2b03bd6257db940
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-24 10:28:20 +0900

    New Project

---
+++ SwiftUI-05/StaticList/StaticList/StaticListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticListApp.swift
+//  StaticList
+//
+//  Created by 차지용 on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct StaticListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
