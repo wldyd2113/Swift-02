5ab9efe054d3824a50929b35cff9bf06751ba9d6
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-25 11:22:34 +0900

    New Project

---
+++ SwiftUI-05/DisclosureGroup/DisclosureGroup/DisclosureGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisclosureGroupApp.swift
+//  DisclosureGroup
+//
+//  Created by 차지용 on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DisclosureGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
