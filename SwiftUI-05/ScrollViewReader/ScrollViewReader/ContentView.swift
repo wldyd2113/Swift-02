abd75eb7c34236b872f914a011cf4d70fcff8ad4
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-25 10:21:50 +0900

    Scroll Programatically

--- SwiftUI-05/ScrollViewReader/ScrollViewReader/ContentView.swift
+++ SwiftUI-05/ScrollViewReader/ScrollViewReader/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var scrolledId: CharacterInfo.ID?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(spacing: 20) {
+            VStack {
+                Text("ios 17+")
+                    .foregroundStyle(.blue)
+                    .font(.title)
+                ScrollView {
+                    Button("Go to letter Q") {
+                        scrolledId = 16
+                    }
+                    .padding()
+                    .background(.blue)
+                    .tint(.yellow)
+                    
+                    ForEach(CharacterInfo.charArray) { image in
+                        Image(systemName: image.name)
+                            .font(.largeTitle)
+                            .foregroundStyle(.blue)
+                            .frame(width: 90, height: 90)
+                            .background(.yellow)
+                            .padding()
+                    }
+                    Button("Go to letter G") {
+                        withAnimation {
+                            scrolledId = 6
+                        }
+                    }
+                }
+                .scrollPosition(id: $scrolledId)
+            }
+//            VStack {
+//                Text("ios 14+")
+//                    .foregroundStyle(.blue)
+//                    .font(.title)
+//                ScrollView {
+//                    
+//                }
+//            }
         }
         .padding()
     }
