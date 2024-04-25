365b6f8036bb16cf37c6e47280b5fc1a4a26b9a9
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-25 11:33:50 +0900

    DisclosureGroup

--- SwiftUI-05/DisclosureGroup/DisclosureGroup/ContentView.swift
+++ SwiftUI-05/DisclosureGroup/DisclosureGroup/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showplanets = true
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            DisclosureGroup(isExpanded: $showplanets) {
+                Text("Mercury")
+                Text("Venus")
+                DisclosureGroup("Earth") {
+                    Text("North America")
+                    Text("South America")
+                    Text("Europe")
+                    Text("Africa")
+                    Text("Asia")
+                    Text("Antarctica")
+                    Text("Oceania")
+                }
+            } label: {
+                Text("Planets")
+            }
+            DisclosureGroup {
+                Text("Surprise! This is an alternative way of using DisclosureGroup")
+            } label: {
+                Label("Tap to reveal", systemImage: "cube.box")
+                    .font(.system(size: 25, design: .rounded))
+                    .foregroundStyle(.blue)
+            }
         }
         .padding()
     }
