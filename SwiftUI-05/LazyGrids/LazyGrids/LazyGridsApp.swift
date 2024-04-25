023a2b8710e0cd3918bb48979e10dad3dd2f1a22
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-25 09:12:41 +0900

    New Project

---
+++ SwiftUI-05/LazyGrids/LazyGrids/LazyGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridsApp.swift
+//  LazyGrids
+//
+//  Created by 차지용 on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
