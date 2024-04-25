b17a9bbcb3269bc2b094927f1efe0b8f09261e79
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-25 09:46:50 +0900

    New Project

---
+++ SwiftUI-05/ScrollViewReader/ScrollViewReader/ScrollViewReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReaderApp.swift
+//  ScrollViewReader
+//
+//  Created by 차지용 on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
