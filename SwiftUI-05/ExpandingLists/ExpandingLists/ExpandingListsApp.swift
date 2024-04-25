b93b3d865b0ee803e0d10b6559bc49eb5290bce0
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-25 11:02:02 +0900

    New Project

---
+++ SwiftUI-05/ExpandingLists/ExpandingLists/ExpandingListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandingListsApp.swift
+//  ExpandingLists
+//
+//  Created by 차지용 on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpandingListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
