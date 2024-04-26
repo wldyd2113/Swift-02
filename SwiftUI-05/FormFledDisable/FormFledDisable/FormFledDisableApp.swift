703bb367b126b2ef050e604e42bd56397fd56fd3
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-26 17:02:23 +0900

    New Project

---
+++ SwiftUI-05/FormFledDisable/FormFledDisable/FormFledDisableApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormFledDisableApp.swift
+//  FormFledDisable
+//
+//  Created by 차지용 on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FormFledDisableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
