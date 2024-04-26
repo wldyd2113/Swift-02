45757aaef2082c489e407f59df97a7972e6df6b3
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-26 11:32:29 +0900

    New Project Traits

---
+++ SwiftUI-05/PreviewingWithTraits/PreviewingWithTraits/PreviewingWithTraitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewingWithTraitsApp.swift
+//  PreviewingWithTraits
+//
+//  Created by 차지용 on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PreviewingWithTraitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
