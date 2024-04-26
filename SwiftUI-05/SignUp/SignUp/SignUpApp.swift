5bb3ca25290a7a02ae2d5246554c613c629b51e8
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-26 16:23:33 +0900

    New Project SignUp

---
+++ SwiftUI-05/SignUp/SignUp/SignUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpApp.swift
+//  SignUp
+//
+//  Created by 차지용 on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SignUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
