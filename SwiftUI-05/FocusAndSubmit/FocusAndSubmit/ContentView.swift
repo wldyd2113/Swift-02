3a4201f073014c2350b5f68002e5eb43384fc55e
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-26 17:37:56 +0900

    Focus

--- SwiftUI-05/FocusAndSubmit/FocusAndSubmit/ContentView.swift
+++ SwiftUI-05/FocusAndSubmit/FocusAndSubmit/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum AddressField {
+        case streetName
+        case city
+        case state
+        case zipCode
+    }
+    
+    @State private var streetName = ""
+    @State private var city = ""
+    @State private var state = ""
+    @State private var zipCode = ""
+    @FocusState private var currentFocus: AddressField?
+
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Address", text: $streetName)
+                .focused($currentFocus, equals: .streetName)
+                .submitLabel(.next)
+            TextField("City", text: $city)
+                .focused($currentFocus, equals: .streetName)
+                .submitLabel(.next)
+            TextField("State", text: $state)
+                .focused($currentFocus, equals: .zipCode)
+                .submitLabel(.next)
+            TextField("ZipCode", text: $zipCode)
+                .focused($currentFocus, equals: .streetName)
+                .submitLabel(.done)
         }
         .padding()
+        .onSubmit {
+            switch currentFocus {
+            case .streetName:
+                currentFocus = .city
+            case .city:
+                currentFocus = .state
+            case .state:
+                currentFocus = .zipCode
+            default:
+                print("Done!!")
+            }
+        }
+        .onAppear {
+            currentFocus = .streetName
+        } //키보드 올라오게해줌
     }
 }
 
