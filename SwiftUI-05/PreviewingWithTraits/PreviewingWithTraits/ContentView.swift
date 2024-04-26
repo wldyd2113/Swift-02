154019d6c63e7ddc63608eded5637ed5d32797c3
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-26 11:41:39 +0900

    Traits Preview

--- SwiftUI-05/PreviewingWithTraits/PreviewingWithTraits/ContentView.swift
+++ SwiftUI-05/PreviewingWithTraits/PreviewingWithTraits/ContentView.swift
@@ -9,16 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.teal
+                .frame(width: 200, height: 150)
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                Text("Hello, world!")
+                    .font(.title2)
+            }
+            .foregroundStyle(.primary)
+            .padding(.all, 20)
+            .background()
+            .border(.tertiary, width: 10)
         }
-        .padding()
     }
 }
 
-#Preview {
+#Preview("Portait") {
+    ContentView()
+}
+
+#Preview("Landscape", traits: .landscapeLeft) {
+    ContentView()
+}
+
+#Preview("Upside down", traits: .portraitUpsideDown) {
+    ContentView()
+}
+
+#Preview(" Size that fits", traits: .sizeThatFitsLayout) {
+    ContentView()
+}
+
+#Preview("Fixed", traits: .fixedLayout(width: 500, height: 200)) {
     ContentView()
 }
