419e17e7f8b0d386a81cd7652db85b421f949987
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-29 10:36:57 +0900

    New Project  RemoteConfig

---
+++ SwiftUI-05/RemoteConfig/RemoteConfig/RemoteConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteConfigApp.swift
+//  RemoteConfig
+//
+//  Created by 차지용 on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct RemoteConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
