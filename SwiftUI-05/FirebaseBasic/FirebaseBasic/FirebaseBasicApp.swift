964538f269683a14a216aa04589e4212d83b14f6
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-29 15:07:00 +0900

    AppDelegate

--- SwiftUI-05/FirebaseBasic/FirebaseBasic/FirebaseBasicApp.swift
+++ SwiftUI-05/FirebaseBasic/FirebaseBasic/FirebaseBasicApp.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 import Firebase
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        GIDSignIn.sharedInstance.handle(url)
+    }
+}
 
 @main
 struct FirebaseBasicApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
