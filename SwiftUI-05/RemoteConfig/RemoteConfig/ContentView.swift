6dddec31cd30f18da2394ef552d99b911cebc341
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-29 13:21:44 +0900

    RemoteConfig functions

--- SwiftUI-05/RemoteConfig/RemoteConfig/ContentView.swift
+++ SwiftUI-05/RemoteConfig/RemoteConfig/ContentView.swift
@@ -10,10 +10,46 @@ import Firebase
 import FirebaseRemoteConfigSwift
 
 class RemoteConfigurateion {
-    private var remteConfig = Firebase.RemoteConfig.remoteConfig()
+    private var remoteConfig = Firebase.RemoteConfig.remoteConfig()
+    init() {
+        //Enable delveper
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+    }
+    
+    private func activate() {
+        remoteConfig.activate { changed, error in
+            guard error == nil else{
+                print("Error activating Remote Config: \(error!.localizedDescription)")
+                return
+            }
+            print("Default value were \(changed ? "" : "NOT")updated from Remote Config")
+        }
+    }
+    func fetchFromServer() async {
+        guard let status = try? await remoteConfig.fetch(), status == .success else {
+            print("Couldn't fetch Remote Config")
+            return
+        }
+        print("Remote Config successfully fetched")
+        activate()
+    }
+    
+    func regiterForRealtimeUpdates() {
+        print("Registering for Remote Config relatime updates")
+        remoteConfig.addOnConfigUpdateListener { [self] update, error in
+            guard let update, error == nil else {
+                print("Error listening for Remote Config realtime updates: \(error!.localizedDescription)")
+                return
+            }
+            print("Updated keys in realtime: \(update.updatedKeys)")
+            activate()
+            
+        }
+    }
+    
 }
-
-
 struct ContentView: View {
     private var config = RemoteConfigurateion()
     @RemoteConfigProperty(key: "screenType", fallback: nil) var screenType: String?
@@ -42,8 +78,13 @@ struct ContentView: View {
             }
         }
         .padding()
+        .task {
+            await config.fetchFromServer()
+            config.regiterForRealtimeUpdates()
+        }
     }
 }
+    
 
 #Preview {
     ContentView()
