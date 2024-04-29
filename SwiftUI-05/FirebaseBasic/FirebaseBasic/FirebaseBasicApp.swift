7138f716572b6c513ed68485db4a090c3648a7b4
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-29 13:29:09 +0900

    New Project FirbaseBasic

---
+++ SwiftUI-05/FirebaseBasic/FirebaseBasic/FirebaseBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBasicApp.swift
+//  FirebaseBasic
+//
+//  Created by 차지용 on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
