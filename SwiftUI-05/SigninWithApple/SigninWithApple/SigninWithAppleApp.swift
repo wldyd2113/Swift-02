4be90001177c6b638ab77ec2c45ee07f7a65980f
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-29 09:13:43 +0900

    App Login

---
+++ SwiftUI-05/SigninWithApple/SigninWithApple/SigninWithAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SigninWithAppleApp.swift
+//  SigninWithApple
+//
+//  Created by 차지용 on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SigninWithAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
