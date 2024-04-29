e07f00a72570f1bbe81b6174af2c97176eac9d5b
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-29 09:43:02 +0900

    SignIn Apple UI

--- SwiftUI-05/SigninWithApple/SigninWithApple/ContentView.swift
+++ SwiftUI-05/SigninWithApple/SigninWithApple/ContentView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
-
+import AuthenticationServices
 struct ContentView: View {
+    @State private var userName: String = ""
+    @State private var userEmail: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.white
+            if userName.isEmpty {
+                SignInWithAppleButton(.signIn,
+                                      onRequest: onRequest,
+                                      onCompletion: onCompletion)
+                .signInWithAppleButtonStyle(.black)
+                .frame(width: 200, height: 50)
+            }
+            else {
+                Text("Welcome\n\(userName), \(userEmail)")
+                    .font(.headline)
+            }
         }
-        .padding()
+    }
+    private func onRequest(_ request: ASAuthorizationAppleIDRequest) {
+        request.requestedScopes = [.fullName, .email]
+    }
+    private func onCompletion(_ result: Result<ASAuthorization, Error>) {
+        switch result {
+        case .success(let authResult):
+            guard let credential = authResult.credential as? ASAuthorizationAppleIDCredential
+            else { return }
+            
+//            TODO: storeData
+        case .failure(let error):
+            print("Authorization failed: " + error.localizedDescription)
+        }
     }
 }
 
