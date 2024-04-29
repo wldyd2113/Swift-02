131c18e539ac50c74d07a9a371678ebfbde74a69
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-29 15:57:12 +0900

    Login logic

--- SwiftUI-05/FirebaseBasic/FirebaseBasic/AuthenticationViewModel.swift
+++ SwiftUI-05/FirebaseBasic/FirebaseBasic/AuthenticationViewModel.swift
@@ -8,13 +8,27 @@
 import Foundation
 import FirebaseAuth
 import GoogleSignIn
+import FirebaseCore
+
+extension UIApplication {
+    static var currentRootViewController: UIViewController? {
+        UIApplication.shared.connectedScenes
+            .filter({ $0.activationState == .foregroundActive })
+            .map({ $0 as? UIWindowScene })
+            .compactMap({ $0 })
+            .first?.windows
+            .filter({ $0.isKeyWindow })
+            .first?
+            .rootViewController
+    }
+}
 
 @Observable
 final class AuthenticationViewModel {
     enum State {
         case busy
         case signedIn
-        case segnedOut
+        case signedOut
     }
     
     var state: State = .busy
@@ -31,11 +45,35 @@ final class AuthenticationViewModel {
     }
     
     func login() {
-        
+        state = .busy
+        guard let clientID = FirebaseApp.app()?.options.clientID,
+              let rootViewController = UIApplication.currentRootViewController else {
+            return
+        }
+        let configuration = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = configuration
+        GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController, hint:nil) {
+            result, error in
+            if let error { print("Error: \(error.localizedDescription)")}
+            Task { await self.signIn(user:result?.user)}
+        }
     }
     
     private func signIn(user: GIDGoogleUser?) async {
+        guard let user, let idToken = user.idToken else {
+            state = .signedOut
+            return
+        }
         
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken.tokenString, accessToken: user.accessToken.tokenString)
+        
+        do {
+            authResult = try await Auth.auth().signIn(with: credential)
+            state = .signedIn
+        } catch {
+            state = .signedOut
+            print("Error: \(error.localizedDescription)")
+        }
     }
 
 }
