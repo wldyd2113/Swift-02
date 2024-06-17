ad255c2c33b182b9a80b51436ad0a780d78e2c4e
jiyong <wldyd2113@gmail.com>
2024-06-17 10:32:33 +0900

    View 구성

--- 07-AsyncCombine/BookShelf/BookShelf/ContentView.swift
+++ 07-AsyncCombine/BookShelf/BookShelf/ContentView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("")
+        HStack(alignment: .top) {
+            Image("9781484285718-M")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 90)
+            VStack(alignment: .leading) {
+                Text("Asynchronous Promming with SwiftUI and Combine")
+                    .font(.headline)
+                Text("by peter Friese")
+                    .font(.subheadline)
+                Text("451 pages")
+                    .font(.subheadline)
+                
+            }
+        }
     }
 }
 
