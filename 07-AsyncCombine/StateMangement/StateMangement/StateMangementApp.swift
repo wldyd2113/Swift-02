aa3502c2606466b1ea389eda56112b1f92321707
jiyong <wldyd2113@gmail.com>
2024-06-17 13:14:29 +0900

    Create StateMangement

---
+++ 07-AsyncCombine/StateMangement/StateMangement/StateMangementApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateMangementApp.swift
+//  StateMangement
+//
+//  Created by 차지용 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct StateMangementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
