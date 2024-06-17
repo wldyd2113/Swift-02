a9b88e11fe3291c6d2426d8d274786807ce10fcc
jiyong <wldyd2113@gmail.com>
2024-06-17 11:50:25 +0900

    Action Handler 통합

--- 07-AsyncCombine/SimpleView/SimpleView/ContentView.swift
+++ 07-AsyncCombine/SimpleView/SimpleView/ContentView.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
+private class PersonViewModel: ObservableObject {
+    @Published var firstName = ""
+    @Published var lastName = ""
+    
+    func save() {
+        print("Save to disk")
+    }
+}
 
+struct ContentView: View {
+    @State var dirty = false
+    @StateObject private var viewModel = PersonViewModel()
+    
+    
+    var body: some View {
+        Form {
+            Section("\(self.dirty ? "*" : "") Input fuelds") {
+                TextField("First name", text: $viewModel.firstName)
+                    .onChange(of: viewModel.firstName) {
+                        self.dirty = true
+                    }
+                    .onSubmit {
+                        viewModel.save()
+                    }
+                TextField("First name", text: $viewModel.lastName)
+                    .onChange(of: viewModel.lastName) {
+                        self.dirty = false
+                    }
+            }
+        }
     }
 }
 
