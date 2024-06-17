b1e4a1b69879cb9e1eafd2154ac2e61e0efbba65
jiyong <wldyd2113@gmail.com>
2024-06-17 14:12:23 +0900

    기본 뷰 생성

---
+++ 07-AsyncCombine/StateMangement/StateMangement/SettingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SettingScreen.swift
+//  StateMangement
+//
+//  Created by 차지용 on 6/17/24.
+//
+
+import SwiftUI
+
+struct SettingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingScreen()
+}
