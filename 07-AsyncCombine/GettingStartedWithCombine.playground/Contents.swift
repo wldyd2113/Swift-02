d449538474834fb5e8e98cd4a5a0260bde3fb7c9
jiyong <wldyd2113@gmail.com>
2024-06-18 11:36:17 +0900

    Combine

--- 07-AsyncCombine/GettingStartedWithCombine.playground/Contents.swift
+++ 07-AsyncCombine/GettingStartedWithCombine.playground/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+Just(42)
+    .print()
+    .sink { value in
+        print("Received \(value)")
+    }
+["Pepper", "Mushrooms", "Onions"].publisher
+    .sink { topping in
+        print("\(topping) is a favorite topping for pizza")
+    }
+"Combine".publisher
+    .sink { value in
+        print("Hello, \(value)")
+    }
+Just("Combine")
+    .sink { value in
+        print("Hello, \(value)")
+    }
