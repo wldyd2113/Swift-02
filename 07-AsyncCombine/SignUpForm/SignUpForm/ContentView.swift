68ebe6e5d62319e0b8f9ba9ec5f23e1bfaf5ed24
jiyong <wldyd2113@gmail.com>
2024-06-18 16:20:28 +0900

    SignUpForm 뷰모델, 뷰 추가

--- 07-AsyncCombine/SignUpForm/SignUpForm/ContentView.swift
+++ 07-AsyncCombine/SignUpForm/SignUpForm/ContentView.swift
@@ -7,15 +7,49 @@
 
 import SwiftUI
 
+class SignUpFormViewModel: ObservableObject {
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var passwordConfirmation: String = ""
+    
+    @Published var usernameMessage: String = ""
+    @Published var passwordMessage: String = ""
+    @Published var isValid: Bool = false
+
+    
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = SignUpFormViewModel()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section(content:  {
+                TextField("Username", text: $viewModel.username)
+                    .textInputAutocapitalization(.none)
+                    .autocorrectionDisabled()//자동완성
+            },footer: {
+                Text(viewModel.usernameMessage)
+                    .foregroundStyle(Color.red)
+            })
+            // Password
+            Section {
+                SecureField("Password",
+                            text: $viewModel.password)
+                SecureField("Repeat password",
+                            text: $viewModel.passwordConfirmation)
+            } footer: {
+                Text(viewModel.passwordMessage)
+                    .foregroundColor(.red)
+            }
+            // Submit button
+            Section {
+                Button("Sign up") {
+                    print("Signing up as \(viewModel.username)")
+                }
+                .disabled(!viewModel.isValid)
+            }
+            
         }
-        .padding()
     }
 }
 
