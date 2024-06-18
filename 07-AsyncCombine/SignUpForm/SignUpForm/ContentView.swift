fb1697f6727b5b1cab4b5f98613ece93da8f6123
jiyong <wldyd2113@gmail.com>
2024-06-18 16:33:35 +0900

    Combine framework

--- 07-AsyncCombine/SignUpForm/SignUpForm/ContentView.swift
+++ 07-AsyncCombine/SignUpForm/SignUpForm/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 class SignUpFormViewModel: ObservableObject {
     @Published var username: String = ""
@@ -16,9 +17,13 @@ class SignUpFormViewModel: ObservableObject {
     @Published var passwordMessage: String = ""
     @Published var isValid: Bool = false
     
+    private lazy var isUsernameLengthValidPublisher: AnyPublisher<Bool, Never> = {
+        $username.map{ $0.count >= 3 }.eraseToAnyPublisher()
+    }()
+    
     init() {
-        $username.map { $0.count >= 3 }.assign(to: &$isValid)
-        $username.map { $0.count >= 3 ? "" : "Username must be at least three characters!"}.assign(to: &$usernameMessage)
+        isUsernameLengthValidPublisher.assign(to: &$isValid)
+        isUsernameLengthValidPublisher.map { $0 ? "" : "Username must be at least three characters!"}.assign(to: &$usernameMessage)
     }
     
 }
