959c9b274546c4097f5ee98459e3ae524d0a9864
jiyong <wldyd2113@gmail.com>
2024-06-18 16:05:31 +0900

    Create SignUpForm

---
+++ 07-AsyncCombine/SignUpForm/SignUpForm/SignUpFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpFormApp.swift
+//  SignUpForm
+//
+//  Created by 차지용 on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SignUpFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
