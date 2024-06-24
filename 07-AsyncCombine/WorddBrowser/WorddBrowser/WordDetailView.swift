6a67c82fe47792dcf4212f28bd98f4964cb78cf1
jiyong <wldyd2113@gmail.com>
2024-06-24 17:07:19 +0900

    WordDetailView, ViewModel 추가

---
+++ 07-AsyncCombine/WorddBrowser/WorddBrowser/WordDetailView.swift
@@ -0,0 +1,45 @@
+//
+//  WordDetailView.swift
+//  WorddBrowser
+//
+//  Created by 차지용 on 6/24/24.
+//
+
+import SwiftUI
+
+struct WordDetailView: View {
+    @State var word: String
+    @StateObject var viewModel = WordDetailViewModel()
+    var body: some View {
+        ZStack {
+            if viewModel.isSearching {
+                ProgressView("Fetching...")
+            }
+            else {
+                List {
+                    Section("Definitions") {
+                        ForEach(viewModel.definitions) {
+                            definition in
+                            HStack {
+                                VStack(alignment: .leading){
+                                    Text("\(definition.partOfSpeech)")
+                                        .font(.caption)
+                                    Text(definition.definition)
+                                }
+                                Spacer()
+                            }
+                        }
+                    }
+                    .lineLimit(2)
+                }
+            }
+        }
+        .navigationTitle(word)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        WordDetailView(word: "Swift")
+    }
+}
---
+++ 07-AsyncCombine/WorddBrowser/WorddBrowser/WordDetailViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  WordDetailViewModel.swift
+//  WorddBrowser
+//
+//  Created by 차지용 on 6/24/24.
+//
+
+import Foundation
+import Combine
+
+class WordDetailViewModel: ObservableObject {
+    @Published private var result = Word.empty
+    @Published var isSearching = false
+    @Published var definitions = [Definition]()
+    
+    
+}
