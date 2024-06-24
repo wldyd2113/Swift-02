04b2768a4b4023a0cbb5d96f19089d7cd9da7b3a
jiyong <wldyd2113@gmail.com>
2024-06-24 13:11:20 +0900

    Create WordBrowser

---
+++ 07-AsyncCombine/WorddBrowser/WorddBrowser/WorddBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorddBrowserApp.swift
+//  WorddBrowser
+//
+//  Created by 차지용 on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WorddBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
