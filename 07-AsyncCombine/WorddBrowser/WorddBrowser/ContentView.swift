42ab38f81b58ae2e3914b38887bd5efa4adbb343
jiyong <wldyd2113@gmail.com>
2024-06-24 13:42:17 +0900

    기본 뷰, 뷰모델 추가

--- 07-AsyncCombine/WorddBrowser/WorddBrowser/ContentView.swift
+++ 07-AsyncCombine/WorddBrowser/WorddBrowser/ContentView.swift
@@ -7,18 +7,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SectionView: View {
+    var title: String
+    var words: [String]
+
+    init(_ title: String, words: [String]) {
+        self.title = title
+        self.words = words
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Section(title) {
+            if words.isEmpty {
+                Text("(No items match your filter criteria)")
+            } else {
+                ForEach(words, id: \.self) { word in
+                    Text(word)
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = LibraryViewModel()
+    var body: some View {
+        List {
+            SectionView("Rondom word", words: [viewModel.randomWord])
+            SectionView("Peter's Tips", words: viewModel.filteredTips)
+            SectionView("My favorites", words: viewModel.favorites)
+        }
+        .searchable(text: $viewModel.searchText)
+        .textInputAutocapitalization(.never)
+        .navigationTitle("")
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button (action:{}){
+                    Image(systemName: "plus")
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
---
+++ 07-AsyncCombine/WorddBrowser/WorddBrowser/LibraryViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  LibraryViewModel.swift
+//  WorddBrowser
+//
+//  Created by 차지용 on 6/24/24.
+//
+
+import Foundation
+
+class LibraryViewModel: ObservableObject {
+    @Published var searchText = ""
+    @Published var randomWord = "partially"
+    @Published var tips: [String] = ["Swift", "authentication", "authorization"]
+    @Published var favorites: [String] = ["stunning", "brilliant", "marvelous"]
+    
+    @Published var filteredTips = [String]()
+    @Published var filteredFavorites = [String]()
+
+}
