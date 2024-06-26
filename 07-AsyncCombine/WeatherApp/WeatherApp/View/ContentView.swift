e39016264f8717534704788b6b30611b1d43851e
jiyong <wldyd2113@gmail.com>
2024-06-26 13:09:13 +0900

    UI작업

--- 07-AsyncCombine/WeatherApp/WeatherApp/View/ContentView.swift
+++ 07-AsyncCombine/WeatherApp/WeatherApp/View/ContentView.swift
@@ -11,31 +11,73 @@ struct ContentView: View {
     @StateObject var viewModel = WeatherViewModel()
     
     var body: some View {
-        VStack(spacing: 20) {
-            Button {
-                print("get location")
-                viewModel.requestLocation()
-            } label: {
-                Text("Get Location")
-            }
-            Button {
-                print("get weather")
-                Task {
-                    if let location = viewModel.location {
-                        await viewModel.fetchWeather(for: location)
-                    } else {
-                        print("no location")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            ScrollView {
+                VStack {
+                    Text("Seongnam-si")
+                        .font(.system(size: 40))
+                    Text("21°")
+                        .font(.system(size: 110, weight: .thin))
+                    Text("Partly Cloudy")
+                        .font(.system(size: 25))
+                    HStack {
+                        Text("H:29°")
+                        Text("L:15°")
                     }
+                    .font(.system(size: 23))
+                    Text("")
+                        .padding(.top)
+                    Text("Cloudy conditions from 1AM-9AM, with showers expected at 9AM.")
+                    ScrollView(.horizontal) {
+                        LazyHStack {
+                            ForEach(0..<10) { index in
+                                VStack {
+                                    Text("\(index)PM")
+                                    Spacer()
+                                    Image(systemName: "cloud.fill")
+                                        .font(.largeTitle)
+                                    Spacer()
+                                    Text("21°")
+                                        .font(.system(size: 21, weight: .semibold))
+                                }
+                            }
+                        }
+                    }
+                    .frame(height: 120)
+                    
+                    Section {
+                        VStack(alignment: .leading) {
+                            ForEach(0..<10) { index in
+                                VStack {
+                                    HStack {
+                                        Text("Mon")
+                                        Image(systemName: "sun.max.fill")
+                                        Text("15°")
+                                            .foregroundStyle(.gray)
+                                        ProgressView(value: 0.5)
+                                            .tint(Color.orange)
+                                        Text("29°")
+                                    }
+                                }
+                                .padding(4)
+                                .font(.system(size: 21, weight: .semibold))
+                                .foregroundStyle(.white)
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    } header: {
+                        HStack {
+                            Label("10-DAY FORCAST", systemImage: "calendar")
+                            Spacer()
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
                 }
-            } label: {
-                Text("Get Weather")
-            }
-            if let error = viewModel.error {
-                Text("\(error)")
-                    .foregroundStyle(.red)
+                .foregroundStyle(.white)
+                .padding()
             }
         }
-        .padding()
     }
 }
 
