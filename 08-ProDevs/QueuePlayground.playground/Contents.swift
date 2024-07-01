4dfe854710dde6a18cb1f70e84e6280bc79f82b5
jiyong <wldyd2113@gmail.com>
2024-07-01 14:32:00 +0900

    DispatchQueue async

---
+++ 08-ProDevs/QueuePlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+let queue1 = DispatchQueue(label: "queue1")
+let queue2 = DispatchQueue(label: "queue2")
+let queue3 = DispatchQueue(label: "queue3")
+
+queue1.async { () -> Void in
+    print(queue1.label)
+}
+
+queue2.async { () -> Void in
+    print(queue2.label)
+}
+
+queue2.async { () -> Void in
+    print(queue2.label)
+}
+
+print("Program stopped")
