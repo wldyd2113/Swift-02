dbd64f5228be201b4d4b5243f4f0c2c1bb47b41d
jiyong <wldyd2113@gmail.com>
2024-07-01 14:35:03 +0900

    sync Queue

--- 08-ProDevs/QueuePlayground.playground/Contents.swift
+++ 08-ProDevs/QueuePlayground.playground/Contents.swift
@@ -4,15 +4,15 @@ let queue1 = DispatchQueue(label: "queue1")
 let queue2 = DispatchQueue(label: "queue2")
 let queue3 = DispatchQueue(label: "queue3")
 
-queue1.async { () -> Void in
+queue1.sync { () -> Void in
     print(queue1.label)
 }
 
-queue2.async { () -> Void in
+queue2.sync { () -> Void in
     print(queue2.label)
 }
 
-queue2.async { () -> Void in
+queue2.sync { () -> Void in
     print(queue2.label)
 }
 
