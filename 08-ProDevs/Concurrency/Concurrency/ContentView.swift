4cf5f4440b8201b791e39a44f122df7651634971
jiyong <wldyd2113@gmail.com>
2024-07-01 16:28:53 +0900

    ConcurrencyApp

--- 08-ProDevs/Concurrency/Concurrency/ContentView.swift
+++ 08-ProDevs/Concurrency/Concurrency/ContentView.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
+enum Response {
+    case success
+}
+
 struct ContentView: View {
+    @State var message = ""
+    @State var sliderValue = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Click Me") {
+                let startTime = NSDate()
+                callFuntion()
+                let endTime = NSDate()
+                message = "Completed in \(endTime.timeIntervalSince(startTime as Date)) seconds"
+            }
+            Spacer()
+            Slider(value: $sliderValue)
+            Text("Message = \(message)")
         }
         .padding()
+    }
+    
+    func doSomething() async throws -> Response {
+        Thread.sleep(forTimeInterval: 20)
+        print("sleep done.")
+        return Response.success
+    }
+    
+    func callFuntion() {
+        Task(priority: .high) {
+            do {
+                _ = try await doSomething()
+            }
+            catch {
+                
+            }
+        }
     }
 }
 
