860ba325e7fb0841e5e1ce394059d7e156b7cb94
jiyong <wldyd2113@gmail.com>
2024-07-01 14:07:47 +0900

    Creat Thread

---
+++ 08-ProDevs/ThreadApp/ThreadApp/ThreadAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadAppApp.swift
+//  ThreadApp
+//
+//  Created by 차지용 on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
