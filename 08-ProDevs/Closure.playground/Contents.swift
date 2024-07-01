77e392be1d7c3c214ba352b53d31da9a7fce5b33
jiyong <wldyd2113@gmail.com>
2024-07-01 11:25:45 +0900

    클로저 문법 생략

--- 08-ProDevs/Closure.playground/Contents.swift
+++ 08-ProDevs/Closure.playground/Contents.swift
@@ -18,3 +18,17 @@ let y = { (x: Int) -> Int in return x * 2 }
 print(y(4))
 print(y(17))
 
+//클로저, 인풋, 아웃풋 인자 생략
+let z = { x in return x * 2}
+print(z(4))
+print(z(17))
+
+//return 키워드 생략
+let w = { x in x * 2}
+print(w(4))
+print(w(17))
+
+//인풋 argument 이름 생략
+let v = { $0 * 2 }
+print(v(4))
+print(v(17))
