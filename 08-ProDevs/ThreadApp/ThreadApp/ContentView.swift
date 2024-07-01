a8053d789201b2e768f056c9c5bd08e38528e1df
jiyong <wldyd2113@gmail.com>
2024-07-01 14:27:43 +0900

    ThreadAPP

--- 08-ProDevs/ThreadApp/ThreadApp/ContentView.swift
+++ 08-ProDevs/ThreadApp/ThreadApp/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var message = ""
+    @State var results = ""
+    @State var sliderValue = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Click Me") {
+                let startTime = NSDate()
+                let fetchedData = fetchSomethingFromServer()
+                let processedData = processData(fetchedData)
+                let firstResult = calculateFirstResult(processedData)
+                let secondResult = calculateSecondResult(processedData)
+                let resultsSummary = "First: [\(firstResult)]\nSecond: [\(secondResult)]"
+                results = resultsSummary
+
+                let endTime = NSDate()
+                message = "Completed in \(endTime.timeIntervalSince(startTime as Date)) seconds."   
+            }
+            TextEditor(text: $results)
+            Slider(value: $sliderValue)
+            Text("Message = \(message)")
         }
         .padding()
+    }
+    
+    func fetchSomethingFromServer() -> String {
+        Thread.sleep(forTimeInterval: 1)
+        return "Hi there"
+    }
+    
+    func processData(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 2)
+        return data.uppercased()
+    }
+    
+    func calculateFirstResult(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 3)
+        let message = "Number of chars: \(String(data).count)"
+        return message
+    }
+    
+    func calculateSecondResult(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 4)
+        return data.replacingOccurrences(of: "E", with: "e")
     }
 }
 
