0f0c7d8717da779db9c04fd0ca10f0b990fee006
jiyong <wldyd2113@gmail.com>
2024-07-02 16:14:14 +0900

    Create StateObjectApp

---
+++ 08-ProDevs/StateObjectApp/StateObjectApp/StateObjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectAppApp.swift
+//  StateObjectApp
+//
+//  Created by 차지용 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
