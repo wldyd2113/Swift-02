33cde1ea96d9224e067d9fbfb7ea4c8d047ce485
jiyong <wldyd2113@gmail.com>
2024-07-02 16:27:11 +0900

    StateObject 데이터 연동

---
+++ 08-ProDevs/StateObjectApp/StateObjectApp/DisplayTextField.swift
@@ -0,0 +1,32 @@
+//
+//  DisplayTextField.swift
+//  StateObjectApp
+//
+//  Created by 차지용 on 7/2/24.
+//
+
+import SwiftUI
+
+struct DisplayTextField: View {
+    @ObservedObject var creature: AnimalModel
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Name:")
+                TextField("Type a name here", text: $creature.name)
+            }
+            HStack {
+                Text("Breed:")
+                TextField("Type a breed here", text: $creature.breed)
+            }
+            HStack {
+                Text("Age:")
+                Slider(value: $creature.age, in: 0...20)
+            }
+            HStack {
+                Text("Weight:")
+                Slider(value: $creature.weight, in: 0...200)
+            }
+        }
+    }
+}
