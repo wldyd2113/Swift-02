b5b9f949e4cccdac90b367f2e03f1a40aec4bb6b
jiyong <wldyd2113@gmail.com>
2024-07-02 10:05:09 +0900

    Create ReadWriteApp

---
+++ 08-ProDevs/ReadWrite/ReadWrite/ReadWriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadWriteApp.swift
+//  ReadWrite
+//
+//  Created by 차지용 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadWriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
