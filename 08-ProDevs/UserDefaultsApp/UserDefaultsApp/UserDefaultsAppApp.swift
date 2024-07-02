2ae1d72fbf3f1ccb3766f288df752ae603899914
jiyong <wldyd2113@gmail.com>
2024-07-02 09:34:20 +0900

    UserDefaults

---
+++ 08-ProDevs/UserDefaultsApp/UserDefaultsApp/UserDefaultsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsAppApp.swift
+//  UserDefaultsApp
+//
+//  Created by 차지용 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
