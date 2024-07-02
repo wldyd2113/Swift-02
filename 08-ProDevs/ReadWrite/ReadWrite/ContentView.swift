98061bb320ee1932153dee716712fd2a84907028
jiyong <wldyd2113@gmail.com>
2024-07-02 10:47:21 +0900

    주석문 추가

--- 08-ProDevs/ReadWrite/ReadWrite/ContentView.swift
+++ 08-ProDevs/ReadWrite/ReadWrite/ContentView.swift
@@ -12,15 +12,21 @@ struct ContentView: View {
     @State var createText = "텍스트를 입력하세요"
     @State var displayText = ""
     
+    //입력에 대한 포커스 상태 변수
     @FocusState var textFieldFocus: Bool
     var body: some View {
         VStack {
             TextEditor(text: $createText)
+                //플레이스 홀더가 표시되어있으면, 텍스트 색상을 회색으로 변경
                 .foregroundStyle(placeHolder == createText ? .gray : .black)
                 .focused($textFieldFocus)
                 .onChange(of: textFieldFocus) {
+                    //텍스트 필드에 포코스 이동시 플레이스홀더가 표시되어 있으면
+                    //입력 텍스트 필드를 초기화하고 입력 대기상태로 만듬
                     if textFieldFocus, placeHolder == createText {
                         createText = ""
+                    //다른 입력으로 포커스 이동시, 입력한 텍스트가 없으면
+                    // 플레이스홀더 기본 텍스트로 대치
                     }
                     else if !textFieldFocus, createText.isEmpty {
                         createText = placeHolder
