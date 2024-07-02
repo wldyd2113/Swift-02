c4c7d82ef7d42e36f4003e232e44b74dc0896f92
jiyong <wldyd2113@gmail.com>
2024-07-02 15:35:17 +0900

    초기 UI

---
+++ 08-ProDevs/BindingApp/BindingApp/BindingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingAppApp.swift
+//  BindingApp
+//
+//  Created by 차지용 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BindingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 08-ProDevs/BindingApp/BindingApp/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  BindingApp
+//
+//  Created by 차지용 on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = ""
+
+    var body: some View {
+        VStack {
+            Text("Hello, \(message)")
+            HStack {
+                Text("Send a greeting:")
+                TextField("Type a message here", text: $message)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
