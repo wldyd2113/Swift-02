4835d9b5d0fdae41451d173706f22f21afb52d3c
jiyong <wldyd2113@gmail.com>
2024-07-02 10:25:13 +0900

    FileReadWrite

--- 08-ProDevs/ReadWrite/ReadWrite/ContentView.swift
+++ 08-ProDevs/ReadWrite/ReadWrite/ContentView.swift
@@ -14,10 +14,30 @@ struct ContentView: View {
         VStack {
             TextEditor(text: $createText)
             HStack {
-                Button(action: {}) {
+                Button(action: {
+                    let fm = FileManager.default
+                    let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
+                    let url = urls.last?.appendingPathComponent("file.tex")
+                    do {
+                        try createText.write(to: url!, atomically: true, encoding: String.Encoding.utf8)
+                    }
+                    catch {
+                        print("File writing error")
+                    }
+                }) {
                     Text("Write File")
                 }
-                Button(action: {}) {
+                Button(action: {          
+                    let fm = FileManager.default
+                    let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
+                    let url = urls.last?.appendingPathComponent("file.txt")
+                    do {
+                        let fileContent = try String(contentsOf: url!, encoding: String.Encoding.utf8)
+                        displayText = fileContent
+                    } catch {
+                        print("File reading error")
+                    }
+                }) {
                     Text("Read File")
                 }
             }
