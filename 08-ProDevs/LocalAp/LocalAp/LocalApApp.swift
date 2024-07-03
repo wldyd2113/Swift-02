2718f941ff9d9a651f11e36ec60fc3628b45bf82
jiyong <wldyd2113@gmail.com>
2024-07-03 09:22:58 +0900

    Create LocalApp

---
+++ 08-ProDevs/LocalAp/LocalAp/LocalApApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalApApp.swift
+//  LocalAp
+//
+//  Created by 차지용 on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalApApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
