67f862da7f745180641d64ece3dbffd611c73b0d
jiyong <wldyd2113@gmail.com>
2024-07-03 11:04:39 +0900

    create PseuDoLanguageApp

---
+++ 08-ProDevs/PseuDoLanguageApp/PseuDoLanguageApp/PseuDoLanguageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PseuDoLanguageAppApp.swift
+//  PseuDoLanguageApp
+//
+//  Created by 차지용 on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PseuDoLanguageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
