fcde53a0e7c0d8b8df4582c3b4c3a7cbb77867a2
jiyong <wldyd2113@gmail.com>
2024-07-03 14:12:48 +0900

    create motionapp

---
+++ 08-ProDevs/MotionApp/MotionApp/MotionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionAppApp.swift
+//  MotionApp
+//
+//  Created by 차지용 on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MotionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
