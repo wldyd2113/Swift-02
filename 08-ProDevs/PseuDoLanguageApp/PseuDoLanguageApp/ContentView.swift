018dfe680257ec909516252ee80c04e1d56fad69
jiyong <wldyd2113@gmail.com>
2024-07-03 11:05:53 +0900

    Text 추가

--- 08-ProDevs/PseuDoLanguageApp/PseuDoLanguageApp/ContentView.swift
+++ 08-ProDevs/PseuDoLanguageApp/PseuDoLanguageApp/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+                .padding()
+                .background(Color.yellow)
+
+            Text("This is a lot of text to display in a small amount of space to test how well Xcode can truncate large amounts of text.")
+                .padding()
+                .background(Color.mint)
         }
         .padding()
     }
