b7b6b933bcf0175ab9b0b5fea5ef82cf1a1b9bd9
jiyong <wldyd2113@gmail.com>
2024-07-03 13:18:56 +0900

    SearchScope

--- 08-ProDevs/SearchApp/SearchApp/ContentView.swift
+++ 08-ProDevs/SearchApp/SearchApp/ContentView.swift
@@ -7,11 +7,19 @@
 
 import SwiftUI
 
+enum ProductScope {
+    case fruit
+    case vegetable
+}
+
+
 struct ContentView: View {
+    @State private var scope: ProductScope = .fruit
+
     @State var searchText = ""
     
     let petArray = ["Cat", "Dog", "Fish", "Donkey", "Canary", "Camel", "Frog"]
-    
+        
     var body: some View {
         NavigationStack {
             PetListView(animals: petArray)
@@ -21,16 +29,21 @@ struct ContentView: View {
         .searchable(text: $searchText,
                     placement: .navigationBarDrawer(displayMode: .always),
                     prompt: "Look for a pet") {
-            Text("Singing").searchCompletion("Canary")
-            Text("Croaking").searchCompletion("Frog")
-            Text("Grumpy").searchCompletion("Cat")
-            Divider()
+//            Text("Singing").searchCompletion("Canary")
+//            Text("Funny").searchCompletion("Canary")
+//            Text("Croaking").searchCompletion("Frog")
+//            Text("Grumpy").searchCompletion("Cat")
+            
             // searchText 가 비었을 경우, hasPrefix는 true 를 리턴
             // 배열의 모든 요소가 출력됨
             ForEach(petArray.filter {  $0.hasPrefix(searchText) }, id: \.self) { name in
                 Text(name)
             }
         }
+                    .searchScopes($scope) {
+                        Text("Fruit").tag(ProductScope.fruit)
+                        Text("Vegetable").tag(ProductScope.vegetable)
+                    }
     }
 }
 
@@ -43,7 +56,6 @@ struct PetListView: View {
     var body: some View {
         List(animals, id: \.self) { animal in
             Text(animal)
-                .background(.yellow)
         }
         .listStyle(.plain)
     }
