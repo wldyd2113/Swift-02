a56b630c1d284a5ab219d3234cf4ead1f677be1e
jiyong <wldyd2113@gmail.com>
2024-07-03 14:13:01 +0900

    Motion App

--- 08-ProDevs/MotionApp/MotionApp/ContentView.swift
+++ 08-ProDevs/MotionApp/MotionApp/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import CoreMotion
 
 struct ContentView: View {
+    let motionManager = CMMotionManager()
+    let queue = OperationQueue()
+    
+    @State private var x: Double = 0.0
+    @State private var y: Double = 0.0
+    @State private var z: Double = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("x: \(x)")
+            Text("y: \(y)")
+            Text("z: \(z)")
         }
         .padding()
+        .onAppear {
+            motionManager.startAccelerometerUpdates(to: queue) {
+                (data: CMAccelerometerData?, error: Error?) in
+                guard let data = data else {
+                    print("error: \(error!)")
+                    return
+                }
+                
+                let trackMotion: CMAcceleration = data.acceleration
+                motionManager.accelerometerUpdateInterval = 2.5
+                DispatchQueue.main.async {
+                    x = trackMotion.x
+                    y = trackMotion.y
+                    z = trackMotion.z
+                }
+
+            }
+        }
     }
 }
 
