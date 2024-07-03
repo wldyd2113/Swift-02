911b0d2172904b338e793da76823ba2cf0a7440d
jiyong <wldyd2113@gmail.com>
2024-07-03 11:17:02 +0900

    Create SearchApp

---
+++ 08-ProDevs/SearchApp/SearchApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchApp
+//
+//  Created by 차지용 on 7/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 08-ProDevs/SearchApp/SearchApp/SearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppApp.swift
+//  SearchApp
+//
+//  Created by 차지용 on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
