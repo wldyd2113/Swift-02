a326b215a7841edca5e45c95000bce46c10e2b68
jiyong <wldyd2113@gmail.com>
2024-07-04 09:37:27 +0900

    지도추가, 위치정보 추가

--- 08-ProDevs/MapApp/MapApp/ContentView.swift
+++ 08-ProDevs/MapApp/MapApp/ContentView.swift
@@ -6,16 +6,25 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    let locationManager = CLLocationManager()
+    
+    @State var message = "Map of Paris"
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.856613, longitude: 2.352222), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Map(coordinateRegion: $region)
+            TextEditor(text: $message)
+                .frame(width: .infinity, height: 100)
         }
-        .padding()
+        .onAppear {
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.distanceFilter = kCLDistanceFilterNone
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
     }
 }
 
