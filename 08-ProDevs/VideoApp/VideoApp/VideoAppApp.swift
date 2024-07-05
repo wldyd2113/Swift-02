845e111c3b3ae047048037d37e75d5c694dc3493
jiyong <wldyd2113@gmail.com>
2024-07-05 10:39:04 +0900

    Create VideoApp

---
+++ 08-ProDevs/VideoApp/VideoApp/VideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppApp.swift
+//  VideoApp
+//
+//  Created by 차지용 on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
