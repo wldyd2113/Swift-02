e49db619eb3fe8d32a13224f8d61877a2241a0b9
jiyong <wldyd2113@gmail.com>
2024-07-05 10:06:46 +0900

    Create AudioApp

---
+++ 08-ProDevs/AudioApp/AudioApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioApp
+//
+//  Created by 차지용 on 7/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
