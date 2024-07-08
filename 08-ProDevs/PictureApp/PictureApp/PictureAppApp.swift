8eb93c8d60afe86df34232ce8794e39d08e1d597
jiyong <wldyd2113@gmail.com>
2024-07-08 14:38:47 +0900

    Create PictureApp

---
+++ 08-ProDevs/PictureApp/PictureApp/PictureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureAppApp.swift
+//  PictureApp
+//
+//  Created by 차지용 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PictureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
