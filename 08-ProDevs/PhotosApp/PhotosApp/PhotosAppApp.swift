99c8a8419bebba3ec8c33bd63548a6aca8d0c658
jiyong <wldyd2113@gmail.com>
2024-07-08 15:23:22 +0900

    Photos방식의 Image

---
+++ 08-ProDevs/PhotosApp/PhotosApp/PhotosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosAppApp.swift
+//  PhotosApp
+//
+//  Created by 차지용 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
