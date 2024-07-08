8e3371c6a784b2c3122758a56a4ecf4e566ba532
jiyong <wldyd2113@gmail.com>
2024-07-08 09:32:46 +0900

    Create Speech2Text

---
+++ 08-ProDevs/Speech2TextApp/Speech2TextApp/Speech2TextAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speech2TextAppApp.swift
+//  Speech2TextApp
+//
+//  Created by 차지용 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Speech2TextAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
