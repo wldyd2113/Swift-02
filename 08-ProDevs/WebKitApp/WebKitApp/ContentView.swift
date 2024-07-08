b7668723c80c5aa7a247ee72377708637845d0ea
jiyong <wldyd2113@gmail.com>
2024-07-08 11:48:27 +0900

    WebKit

--- 08-ProDevs/WebKitApp/WebKitApp/ContentView.swift
+++ 08-ProDevs/WebKitApp/WebKitApp/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
     var body: some View {
@@ -21,4 +22,17 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: UIViewRepresentableContext<WebView>) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
 }
