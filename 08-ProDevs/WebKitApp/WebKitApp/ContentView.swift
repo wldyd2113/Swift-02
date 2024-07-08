43e7e8136558eb68b0d6279cfbe9290ebe0e03d9
jiyong <wldyd2113@gmail.com>
2024-07-08 11:43:01 +0900

    PdfApp

---
+++ 08-ProDevs/WebKitApp/WebKitApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebKitApp
+//
+//  Created by 차지용 on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
