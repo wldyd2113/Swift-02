8e5738454a3ee680d40a957416b1fbb63025b7f5
jiyong <wldyd2113@gmail.com>
2024-07-08 11:34:38 +0900

    다양한 목소리 테스트

--- 08-ProDevs/PDFApp/PDFApp/ContentView.swift
+++ 08-ProDevs/PDFApp/PDFApp/ContentView.swift
@@ -6,14 +6,13 @@
 //
 
 import SwiftUI
+import PDFKit
 
 struct ContentView: View {
+    let fileURL = Bundle.main.url(forResource: "example", withExtension: "pdf")
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ViewMe(url: fileURL!)
         }
         .padding()
     }
@@ -21,4 +20,19 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+
+struct ViewMe: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> some UIView {
+        let pdfView = PDFView()
+        pdfView.document = PDFDocument(url: url)
+        return pdfView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
 }
