7171e5d4d2629798106b8a7a3bdb3a5c96413603
jiyong <wldyd2113@gmail.com>
2024-07-08 15:10:29 +0900

    UIImagePickerController 사용 실습

--- 08-ProDevs/PictureApp/PictureApp/ContentView.swift
+++ 08-ProDevs/PictureApp/PictureApp/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showSheet = false
+    @State private var image = UIImage()
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+            HStack {
+                Button(action: {
+                    showSheet = true
+                    sourceType = .photoLibrary
+                }) {
+                    Text("Photo library")
+                        .font(.title2)
+                }
+                Spacer()
+                Button(action: {
+                    showSheet = true
+                    sourceType = .camera
+                }) {
+                    Text("Camera")
+                        .font(.title2)
+                }
+            }.padding()
         }
         .padding()
+        .sheet(isPresented: $showSheet) {
+            ImagePicker(sourceType: sourceType, chosenImage: $image)
+        }
     }
 }
 
