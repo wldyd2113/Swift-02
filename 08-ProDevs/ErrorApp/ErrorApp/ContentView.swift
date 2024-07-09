781341d4afa90f08e0a6b57313e126a9bc962434
jiyong <wldyd2113@gmail.com>
2024-07-09 15:35:56 +0900

    do-try-catch 구문

--- 08-ProDevs/ErrorApp/ErrorApp/ContentView.swift
+++ 08-ProDevs/ErrorApp/ErrorApp/ContentView.swift
@@ -7,15 +7,61 @@
 
 import SwiftUI
 
+enum MajorProblems: Error {
+    case divideByzero
+    case noNefativeNumberPlease
+}
+
 struct ContentView: View {
+    
+    @State var message = ""
+    @State var numeratorSlider: Float = 0.0
+    @State var denominatorSlider: Float = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("Numerator = \(numeratorSlider)")
+                Slider(value: $numeratorSlider, in: -20...20, step: 1.0)
+            }
+            HStack {
+                Text("Denominator = \(denominatorSlider)")
+                Slider(value: $denominatorSlider, in: -20...20, step: 1.0)
+            }
+            Button {
+                divideFunction(numerator: Double(numeratorSlider), denominator: Double(denominatorSlider))
+            } label: {
+                Text("Divide")
+            }
+            Text(message)
         }
         .padding()
+    }
+    
+    func divideFunction(numerator: Double, denominator: Double) {
+        do {
+            try checkMe(top: numerator, bottom: denominator)
+            message = "Answer = \(numerator / denominator)"
+        }
+        catch MajorProblems.divideByzero {
+            message = "Can't divide by zero"
+        }
+        catch MajorProblems.noNefativeNumberPlease {
+            message = "No negative numbers, please"
+        }
+        catch {
+            message = "Some other error occurred"
+
+        }
+    }
+    
+    func checkMe(top: Double, bottom: Double) throws {
+        guard(bottom != 0) else {
+            throw MajorProblems.divideByzero
+        }
+        guard(top > 0 && bottom > 0) else {
+            throw MajorProblems.noNefativeNumberPlease
+        }
     }
 }
 
