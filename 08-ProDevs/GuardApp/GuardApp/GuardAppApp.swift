2e714160ffb321fa42c7ba86713ba06b2a52514e
jiyong <wldyd2113@gmail.com>
2024-07-09 14:41:16 +0900

    GuardApp

---
+++ 08-ProDevs/GuardApp/GuardApp/GuardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuardAppApp.swift
+//  GuardApp
+//
+//  Created by 차지용 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GuardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
