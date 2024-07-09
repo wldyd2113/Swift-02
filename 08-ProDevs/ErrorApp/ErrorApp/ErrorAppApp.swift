afcc5da5cf990c26f67ac3f1c4b25e11d813657f
jiyong <wldyd2113@gmail.com>
2024-07-09 15:23:00 +0900

    ErrorApp

---
+++ 08-ProDevs/ErrorApp/ErrorApp/ErrorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorAppApp.swift
+//  ErrorApp
+//
+//  Created by 차지용 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ErrorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
