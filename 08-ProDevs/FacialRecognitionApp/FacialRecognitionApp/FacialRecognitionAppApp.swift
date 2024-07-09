fbcd1c1ae146b25d661116637cae2f1a9df1c169
jiyong <wldyd2113@gmail.com>
2024-07-09 09:20:56 +0900

    FacialRecognitionApp

---
+++ 08-ProDevs/FacialRecognitionApp/FacialRecognitionApp/FacialRecognitionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialRecognitionAppApp.swift
+//  FacialRecognitionApp
+//
+//  Created by 차지용 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FacialRecognitionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
