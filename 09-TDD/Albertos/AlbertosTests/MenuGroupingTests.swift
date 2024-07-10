3c8d61bb9d7dc21419d1ad3f4a8b154ff9a57912
jiyong <wldyd2113@gmail.com>
2024-07-10 13:13:10 +0900

    Test 함수 작성

---
+++ 09-TDD/Albertos/AlbertosTests/MenuGroupingTests.swift
@@ -0,0 +1,26 @@
+//
+//  AlbertosTests.swift
+//  AlbertosTests
+//
+//  Created by 차지용 on 7/10/24.
+//
+
+import XCTest
+@testable import Albertos
+
+
+final class MenuGroupingTests: XCTestCase {
+    func testMenuWithManyCategoriesReturnsOneSectionPerCategory() {
+        
+    }
+    
+    func testMenuWithOneCategoryReturnOneSection() {
+        
+    }
+    
+    func testEmptyMenuReturnEmptySection() {
+        
+    }
+}
+
+
