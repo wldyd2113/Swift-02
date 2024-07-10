c2128b9977b270840a8ae9a1a4b707666cbe927f
jiyong <wldyd2113@gmail.com>
2024-07-10 13:07:11 +0900

    Albertos

---
+++ 09-TDD/Albertos/Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by 차지용 on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
