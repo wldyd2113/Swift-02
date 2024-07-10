19d11b06df434fe301a52b8f3227f16451d5bed2
jiyong <wldyd2113@gmail.com>
2024-07-10 13:46:17 +0900

    MenuItem,MenuSection 구성, 1차 테스트

--- 09-TDD/Albertos/Albertos/MenuItem.swift
+++ 09-TDD/Albertos/Albertos/MenuItem.swift
@@ -9,9 +9,12 @@ import Foundation
 
 struct MenuItem {
     let category: String
+    let name: String
 }
 
 func groupMenuByCategory(_ menu: [MenuItem]) -> [MenuSection] {
     if menu.isEmpty { return [] }
-    return [MenuSection()]
+    return Dictionary(grouping: menu, by: { $0.category} )
+        .map { key, value in MenuSection(category: key, items: value)}
+        .sorted { $0.category > $1.category }
 }
--- 09-TDD/Albertos/Albertos/MenuSection.swift
+++ 09-TDD/Albertos/Albertos/MenuSection.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 struct MenuSection {
-    
+    let category: String
+    let items: [MenuItem]
 }
 
 
