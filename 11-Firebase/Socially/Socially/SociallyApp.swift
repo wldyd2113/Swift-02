3914288dc9c91e80e078c2a3a20cda315f1c01dc
jiyong <wldyd2113@gmail.com>
2024-07-23 13:25:15 +0900

    SociallyApp

---
+++ 11-Firebase/Socially/Socially/SociallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SociallyApp.swift
+//  Socially
+//
+//  Created by 차지용 on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SociallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
